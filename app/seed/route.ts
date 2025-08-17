31168c7fda6f614c5f37956ec2d0b0e1fb120462
glarah453 <glarah453@gmail.com>
2025-08-17 12:19:50 -0400

    Ajuste de Semilla para creacion de base de datos 3

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,8 +1,8 @@
 import bcrypt from 'bcrypt';
 import postgres from 'postgres';
 // import { invoices, customers, revenue, users } from '../lib/placeholder-data';
-import seed_creates_tables from '../db/seed_creacion_tablas';
-import { seed_data_regiones, seed_data_comunas } from '../db/seed_data_db_1';
+import seed_creates_tables from '../../db/seed_creacion_tablas';
+import { seed_data_regiones, seed_data_comunas } from '../../db/seed_data_db_1';
 
 import {
   seed_data_categorias,
@@ -10,13 +10,13 @@ import {
   seed_data_dificultades_escalada,
   seed_data_dificultades_mtb,
   seed_data_dificultades_trekking,
-} from '../db/seed_data_db_2';
+} from '../../db/seed_data_db_2';
 
 import {
   seed_data_subcategorias_dificultades_escalada,
   seed_data_subcategorias_dificultades_mtb,
   seed_data_subcategorias_dificultades_trekking,
-} from '../db/seed_data_db_3';
+} from '../../db/seed_data_db_3';
 
 import {
   createUserProcedures,
@@ -27,7 +27,7 @@ import {
   createComentarioProcedures,
   createValoracionProcedures,
   createMeGustaProcedures,
-} from '../db/seed_creates_procedures'
+} from '../../db/seed_creates_procedures'
 
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
