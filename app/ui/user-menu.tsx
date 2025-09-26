ae6d060a0862d92d7d86ed2f75ab7523818077d8
glarah453 <glarah453@gmail.com>
2025-09-26 12:30:53 -0300

    cambios en la animacion del menu del usuario

--- app/ui/user-menu.tsx
+++ app/ui/user-menu.tsx
@@ -5,9 +5,10 @@ import { useRouter } from "next/navigation";
 import Link from 'next/link';
 import Image from 'next/image';
 import { useAuth } from "@/context/AuthContext";
-import { Menu } from "@headlessui/react";
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment } from "react";
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
-import { PowerIcon,ArrowRightIcon } from '@heroicons/react/24/outline';
+import { PowerIcon,ArrowRightIcon, Cog8ToothIcon } from '@heroicons/react/24/outline';
 // import { logOutUser } from "@/app/lib/actions";
 
 export default function UserMenu() {
@@ -42,48 +43,62 @@ export default function UserMenu() {
           <ChevronDownIcon className="h-4 w-4 text-gray-500" />
         </Menu.Button>
 
-        {/* Dropdown */}
-        <Menu.Items
-          className="absolute right-0 bottom-full mb-2 w-56 origin-bottom-right md:origin-top-right 
-             rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 
-             focus:outline-none z-50"
+        {/* Animación */}
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-200"
+          enterFrom="opacity-0 translate-y-1"
+          enterTo="opacity-100 translate-y-0"
+          leave="transition ease-in duration-100"
+          leaveFrom="opacity-100 translate-y-0"
+          leaveTo="opacity-0 translate-y-1"
         >
-        {/* <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-50"> */}
-          <div className="p-2">
-            <Menu.Item>
-              {({ active }) => (
-                <Link
-                  href="/profile"
-                  className={`block rounded-md px-4 py-2 text-sm ${
-                    active ? "bg-gray-100" : ""
-                  }`}
-                >
-                  Configuración de perfil
-                </Link>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <button
-                  onClick={async () => {
-                    // await logOutUser();
-                    await sign_out();
-                    window.location.href = "/";
-                    // router.push("/");
-                  }}
-                  className={`flex h-[48px] w-full grow items-center justify-center gap-2 
-                    rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 
-                    hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3
-                  ${
-                    active ? "bg-gray-100" : ""
-                  }`}
-                >
-                  <PowerIcon className="w-4" /> Cerrar sesión
-                </button>
-              )}
-            </Menu.Item>
-          </div>
-        </Menu.Items>
+          {/* Dropdown */}
+          <Menu.Items
+            className="absolute right-0 bottom-full mb-2 w-56 origin-bottom-right md:origin-top-right 
+                       rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 
+                       focus:outline-none z-50"
+          >
+          {/* <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-50"> */}
+            <div className="p-2">
+              <Menu.Item>
+                {({ active }) => (
+                  <Link
+                    href="/profile"
+                    // className={`block rounded-md px-4 py-2 text-sm ${
+                    className={`flex h-[48px] w-full grow items-center justify-center gap-2 
+                      rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 
+                      hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3 ${
+                      active ? "bg-gray-100" : ""
+                    }`}
+                  >
+                    <Cog8ToothIcon className="w-4" /> Configuración de perfil
+                  </Link>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    onClick={async () => {
+                      // await logOutUser();
+                      await sign_out();
+                      window.location.href = "/";
+                      // router.push("/");
+                    }}
+                    className={`flex h-[48px] w-full grow items-center justify-center gap-2 
+                      rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 
+                      hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3
+                    ${
+                      active ? "bg-gray-100" : ""
+                    }`}
+                  >
+                    <PowerIcon className="w-4" /> Cerrar sesión
+                  </button>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
       </Menu>
     </div>
   );
